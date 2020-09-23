4b8e31ec545a023289cea1b7d1f143ddb7dc4166
jordiGarcia <jordigarcia81@hotmail.com>
2020-09-23 01:39:16 +0100

    subimos inserts_tablas.sql

---
+++ inserts_tablas.sql
@@ -0,0 +1,6 @@
+INSERT INTO usuarios VALUES ('jordiGarcia81@hotmail.com','Jordi','Garcia Ibor',38);
+INSERT INTO mercados VALUES (1.43, 2.85, 100 ,50,1.5);
+INSERT INTO eventos VALUES (1,'Madrid','2020-09-10',1.5,'Valencia');
+INSERT INTO cuenta VALUES (5678903345996712,23,100.50,'jordiGarcia81@hotmail.com');
+INSERT INTO apuesta VALUES ('over/under',75.99,200.25,'2020-09-10','jordiGarcia81@hotmail.com',1,1.5);
+
