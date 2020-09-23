e1c98913b1044e820aad5c2bd500f4c9780722fb
jordiGarcia <jordigarcia81@hotmail.com>
2020-09-23 11:50:26 +0100

    subimos todos los archivos

--- inserts_tablas.sql
+++ inserts_tablas.sql
@@ -1,6 +1,5 @@
 INSERT INTO usuarios VALUES ('jordiGarcia81@hotmail.com','Jordi','Garcia Ibor',38);
-INSERT INTO mercados VALUES (1.43, 2.85, 100 ,50,1.5);
-INSERT INTO eventos VALUES (1,'Madrid','2020-09-10',1.5,'Valencia');
+INSERT INTO mercados VALUES (1.43,2.85,100.50,50.25,1.5,1,1);
+INSERT INTO eventos VALUES (1,'Madrid','2020-09-10','Valencia');
 INSERT INTO cuenta VALUES (5678903345996712,23,100.50,'jordiGarcia81@hotmail.com');
-INSERT INTO apuesta VALUES ('over/under',75.99,200.25,'2020-09-10','jordiGarcia81@hotmail.com',1,1.5);
-
+INSERT INTO apuesta VALUES ('over/under',75.99,200.25,'2020-09-10','jordiGarcia81@hotmail.com',1,1);
